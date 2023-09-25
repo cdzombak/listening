b4df3e578f91dd9d147ee59f7092b180f783e42d
Chris Dzombak <chris@dzombak.com>
2023-09-25 15:34:53 -0400

    WIP: initial commit w/build process

---
+++ Makefile
@@ -0,0 +1,35 @@
+SHELL:=/usr/bin/env bash
+
+# nb. homebrew-releaser assumes the program name is == the repository name
+BIN_NAME:=listening
+BIN_VERSION:=$(shell ./.version.sh)
+
+default: help
+.PHONY: help  # via https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+help: ## Print help
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: all
+all: clean build ## Clean and build for all supported platforms/architectures
+
+.PHONY: clean
+clean: ## Remove build products (./out)
+	rm -rf ./out
+
+.PHONY: build
+build: ## Build for all supported platforms & architectures to ./out
+	mkdir -p out
+	cp ./listening.py ./out/${BIN_NAME}-${BIN_VERSION}-all
+	sed -i 's/__LISTENING_VERSION__ = "<dev>"/__LISTENING_VERSION__ = "${BIN_VERSION}"/g' ./out/${BIN_NAME}-${BIN_VERSION}-all
+
+.PHONY: package
+package: all ## Build binary + .deb package to ./out (requires fpm: https://fpm.readthedocs.io)
+	fpm -t deb -v ${BIN_VERSION} -p ./out/${BIN_NAME}-${BIN_VERSION}-all.deb ./out/${BIN_NAME}-${BIN_VERSION}-all=/usr/bin/${BIN_NAME}
+
+.PHONY: lint
+lint: ## Lint all source files in this repository (requires nektos/act: https://nektosact.com)
+	act --artifact-server-path /tmp/artifacts -j lint
+
+.PHONY: update-lint
+update-lint: ## Pull updated images supporting the lint target (may fetch >10 GB!)
+	docker pull catthehacker/ubuntu:full-latest
---
+++ listening.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import argparse
+import platform
+import subprocess
+import sys
+
+__LISTENING_VERSION__ = "<dev>"
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def str2bool(v):
+    if v.casefold() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.casefold() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def cmd_macos(port=None, transport=None, ipv=None):
+    return "lsof -nP -i{ipv:s}{transport:s}{port:s}".format(
+        ipv="{:d}".format(ipv) if ipv else "",
+        transport=transport.upper() if transport else "",
+        port=":{:d}".format(port) if port else "",
+    )
+
+
+def cmd_linux(port=None, transport=None, ipv=None):
+    if ipv == 4:
+        ipv_part = ' | grep --color=never -v -e "tcp6" -e "udp6"'
+    elif ipv == 6:
+        ipv_part = ' | grep --color=never -e "tcp6" -e "udp6"'
+    else:
+        ipv_part = ""
+    return "netstat -lnp{transport:s}".format(
+        ipv=ipv_part,
+        transport=transport[0].lower() if transport else "tu",
+        port=' | grep --color=never ":{:d}"'.format(port) if port else "",
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="List processes listening for network connections. "
+        "(Wraps lsof on macOS; netstat on Linux.)",
+        prog="listening",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=None,
+        help="Filter to processes listening on the given port. Default: none (all).",
+    )
+    parser.add_argument(
+        "-t",
+        "--transport",
+        type=str,
+        default=None,
+        choices=["tcp", "udp"],
+        help="Specifies TCP or UDP. Default: none (both).",
+    )
+    parser.add_argument(
+        "-i",
+        "--ip",
+        type=int,
+        default=None,
+        choices=[4, 6],
+        help="Specifies IPv4 or IPv6. Default: none (both).",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        type=str2bool,
+        default="false",
+        help="Whether to print the lsof/netstat command to be executed to stderr. Default: False.",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s " + __LISTENING_VERSION__,
+    )
+    args = parser.parse_args()
+
+    if "darwin" in platform.system().lower():
+        cmd = cmd_macos(port=args.port, transport=args.transport, ipv=args.ip)
+    elif "linux" in platform.system().lower():
+        cmd = cmd_linux(port=args.port, transport=args.transport, ipv=args.ip)
+    else:
+        eprint("Platform {:s} is unsupported.".format(platform.system()))
+        sys.exit(1)
+
+    if args.debug:
+        eprint("+ {:s}".format(cmd))
+        eprint("")
+    p = subprocess.Popen(cmd, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    sys.exit(p.wait())
